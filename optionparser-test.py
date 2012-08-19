bb970c1f7a70af23331b36b4f1b257c3892f7f9c
Anthony DeBarros <adebarros@gmail.com>
2012-08-18 22:02:11 -0400

    Add option parser example.

---
+++ optionparser-test.py
@@ -0,0 +1,18 @@
+# One way to add switches to scripts
+# http://www.alexonlinux.com/pythons-optparse-for-human-beings
+
+from optparse import OptionParser
+
+parser = OptionParser()
+parser.add_option('-f', dest='file', default=False, action='store_true')
+parser.add_option('-d', dest='db', default=False, action='store_true')
+
+(options, args) = parser.parse_args()
+
+if options.file == True:
+    print 'File'
+else: pass
+
+if options.db == True:
+    print 'DB'
+else: pass
