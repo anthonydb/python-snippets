cc50319ea86d5f9ad2a637ac763f676007d052f6
Anthony DeBarros <adebarros@gmail.com>
2013-02-02 22:46:27 -0500

    Flesh out xlrd example.

--- excel-xlrd.py
+++ excel-xlrd.py
@@ -3,7 +3,7 @@
 
 import xlrd
 
-wb = xlrd.open_workbook('your-spreadsheet.xls')
+wb = xlrd.open_workbook('excel-xlrd-sample.xls')
 
 # Print the sheet names
 print wb.sheet_names()
@@ -13,22 +13,22 @@ sh = wb.sheet_by_index(0)
 
 # Iterate through rows, returning each as a list that you can index:
 for rownum in range(sh.nrows):
-   print sh.row_values(rownum)
+    print sh.row_values(rownum)
 
 # If you just want the first column:
 first_column = sh.col_values(0)
 print first_column
 
 # Index individual cells:
-cell_x = sh.cell(3,5).value
-#cell_C4 = sh.cell(rowx=3,colx=2).value
-print cell_x
+cell_c4 = sh.cell(3, 2).value
+# Or you can use:
+#cell_c4 = sh.cell(rowx=3, colx=2).value
+print cell_c4
 
-# Let's say you want same cell from x identical sheets in a workbook:
+# Let's say you want the same cell from x identical sheets in a workbook:
 x = 2
 while x >= 0:
     sh = wb.sheet_by_index(x)
-    cell_x = sh.cell(105,8).value
+    cell_x = sh.cell(2, 3).value
     print cell_x
     x = x - 1
-
