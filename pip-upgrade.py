ecb3b5d78d2c0345d9f25ed4d720d9eae5ac1ac0
adebarros <adebarros@gmail.com>
2013-03-18 22:30:49 -0400

    Code for mass pip update.

---
+++ pip-upgrade.py
@@ -0,0 +1,8 @@
+# Update all your installed Python libraries via pip
+# From http://stackoverflow.com/questions/2720014/upgrading-all-packages-with-pip
+
+import pip
+from subprocess import call
+
+for dist in pip.get_installed_distributions():
+    call("pip install --upgrade " + dist.project_name, shell=True)
