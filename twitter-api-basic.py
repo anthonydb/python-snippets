0bf2e602712c8df0d0bf9abb40c5c332fe2ea4a9
AnthonyDB <adebarros@gmail.com>
2013-07-01 17:48:57 -0400

    Changes for v1.1 of Twitter API.

--- twitter-api-basic.py
+++ twitter-api-basic.py
@@ -6,25 +6,23 @@
 # Use the requests module
 import twitter
 
-api = twitter.Api(consumer_key='your-consumer-key',
-                  consumer_secret='your-consumer-secret',
-                  access_token_key='your-access-token-key',
-                  access_token_secret='your-access-token-secret')
+api = twitter.Api(consumer_key='RtrhKyhOal08VTHWVlEPKw',
+                  consumer_secret='vb7fgiNSiF4x2zZIek2yGPxQfzJgQO6dRYoUeiuhBg',
+                  access_token_key='18682941-4hd33e9dtYVWDScSyhc0dx9AVl7pQ4s7V2EbV8zjV',
+                  access_token_secret='whz6Qc3MUWUtOMqBCr3UhiZ1LiyPahkTiYnD52OWBeQ')
 
 # set a handle
 handle = 'anthonydb'
 
 # Get some info on a user
-user = api.GetUser(handle)
+user = api.GetUser(screen_name=handle)
+
 print user.GetName()
-print user.GetFollowersCount()
 print user.GetDescription()
-
+print user.GetFollowersCount()
+print user.GetStatusesCount()
+print user.GetUrl()
 
 # get a user timeline
-statuses = api.GetUserTimeline('usatoday', count=1)
+statuses = api.GetUserTimeline(screen_name='usatoday', count=1)
 print [s.text for s in statuses]
-
-# get a user timeline
-#statuses = api.GetPublicTimeline()
-#print [s.text for s in statuses]
